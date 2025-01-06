791b4e8bd4778acdc7ceb4f446adc2dc6a92efa6
samneng mx <samneng@marinax.co.jp>
2025-01-06 12:00:54 +0700

    change text

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,16 @@ export const metadata: Metadata = {
   title: 'Salary Tax Calculator',
   description: 'Calculate your salary after tax',
   icons: '/img/tax-calculate.png',
+  openGraph: {
+    type: "website",
+    url: "https://salary-tax-calculate.vercel.app/",
+    title: "Salary Tax Calculator",
+    description: "Calculate your salary after tax deductions and dependants",
+    siteName: "Salary Tax Calculator",
+    images: [{
+      url: "/img/tax-calculate.png",
+    }],
+  }
 }
 
 export default function RootLayout({
